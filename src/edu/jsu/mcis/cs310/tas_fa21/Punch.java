98f3f3d5f726620541c80f74276ebe66629bb781
Anderson Turner <tturner15@stu.jsu.edu>
2021-10-01 12:11:39 -0500

    Changed punch class.

--- src/edu/jsu/mcis/cs310/tas_fa21/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Punch.java
@@ -7,8 +7,42 @@ package edu.jsu.mcis.cs310.tas_fa21;
 
 /**
  *
- * @author Jerem
+ * @author Andy
  */
 public class Punch {
-    
+    //Variables
+
+	private String id; 
+	private String description; 
+
+//Constructor
+
+	public Punch (String id, String description){
+	 this.id = id;
+	 this.description = description;
+	}
+
+//Getters
+
+	public String getId()
+	{
+		return id; 
+	}
+	public String getDescription()
+	{
+		return id; 
+	}
+
+//toString method that puts information into a formated string using StringBuilder
+
+	@Override
+	public String toString(){
+	//String id and description 
+	StringBuilder a = new StringBuilder(); 
+	//appends 
+	a.append('#').append(id).append(' '); 
+	a.append('(').append(description).append(' '); 
+
+	return a.toString(); 
+	}  
 }
