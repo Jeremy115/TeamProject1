c277a7d4a0f60a1bdc39b694a0934293c7d699b2
Jeremy Wilson <Jeremycwilson2000@gmail.com>
2021-10-01 12:07:01 -0500

    Did the Badge java class.

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -10,8 +10,39 @@ package edu.jsu.mcis.cs310.tas_fa21;
  * @author Jerem
  */
 public class Badge {
-    
-    
-    
-    
+    //Variables
+
+	private String id; 
+	private String description; 
+
+//Constructor
+
+	public Badge (String id, String description){
+	 this.id = id;
+	 this.description = description;
+	}
+
+//Getters
+
+	public String getId()
+	{
+		return id; 
+	}
+	public String getDescription()
+	{
+		return id; 
+	}
+
+//toString method that puts information into a formated string using StringBuilder
+
+	@Override
+	public String toString(){
+	//String id and description 
+	StringBuilder a = new StringBuilder(); 
+	//appends 
+	a.append('#').append(id).append(' '); 
+	a.append('(').append(description).append(' '); 
+
+	return a.toString(); 
+	}  
 }
