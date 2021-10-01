6ab25ab466d372dd09bdea905549663e433f6069
fpatel7 <79761974+fpatel7@users.noreply.github.com>
2021-10-01 12:25:11 -0500

    make change in shift class

--- src/edu/jsu/mcis/cs310/tas_fa21/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Shift.java
@@ -10,5 +10,38 @@ package edu.jsu.mcis.cs310.tas_fa21;
  * @author Jerem
  */
 public class Shift {
-    
+    private String id; 
+	private String description; 
+
+//Constructor
+
+	public Shift (String id, String description){
+	 this.id = id;
+	 this.description = description;
+	}
+
+//Getters
+
+	public String getId()
+	{
+		return id; 
+	}
+	public String getDescription()
+	{
+		return id; 
+	}
+
+//toString method that puts information into a formated string using StringBuilder
+
+	@Override
+	public String toString(){
+	//String id and description 
+	StringBuilder a = new StringBuilder(); 
+	//appends 
+	a.append('#').append(id).append(' '); 
+	a.append('(').append(description).append(' '); 
+
+	return a.toString(); 
+	}  
 }
+
