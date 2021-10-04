5a19051fd50f7dee5f5e7d5e872d8b024fd030e1
Jeremy Wilson <Jeremycwilson2000@gmail.com>
2021-10-04 11:49:22 -0500

    Test Commit 2

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.jsu.mcis.cs310.tas_fa21;
 
-/**
- *
- * @author Jerem
- */
 public class Badge {
     //Variables
 
